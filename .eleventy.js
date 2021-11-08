ee06e537ca98dcf3bd108d6cf905a939e6a6affa
projectyelen <projectyelen@tuta.io>
2021-11-09 00:26:35 +0100

    version 1.0

---
+++ .eleventy.js
@@ -0,0 +1,111 @@
+const moment = require('moment');
+const yaml = require("js-yaml");
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const htmlmin = require("html-minifier");
+
+moment.locale('en');
+
+module.exports = function (eleventyConfig) {
+  // Disable automatic use of your .gitignore
+  eleventyConfig.setUseGitIgnore(false);
+
+  // Merge data instead of overriding
+  eleventyConfig.setDataDeepMerge(true);
+
+  // human readable date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+  
+  eleventyConfig.addCollection("pokloni", function (collection) {
+    return collection.getFilteredByTag("pokloni")
+      .filter(item => {
+        return process.env.ELEVENTY_ENV === 'development' || item.data.published || item.data.published === undefined;
+      });
+  });
+
+  // Syntax Highlighting for Code blocks
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  // To Support .yaml Extension in _data
+  // You may remove this if you can use JSON
+  eleventyConfig.addDataExtension("yaml", (contents) =>
+    yaml.safeLoad(contents)
+  );
+
+  // Copy Static Files to /_Site
+  eleventyConfig.addPassthroughCopy({
+    "./src/admin/config.yml": "./admin/config.yml",
+    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
+    "./node_modules/prismjs/themes/prism-tomorrow.css":
+      "./static/css/prism-tomorrow.css",
+  });
+
+  // Copy Image Folder to /_site
+  eleventyConfig.addPassthroughCopy("./src/static/img");
+
+  // Copy favicon to route of /_site
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  
+  
+  
+  eleventyConfig.addFilter('datePretty', date => {
+      return moment(date).format('DD MMMM YYYY')
+  });
+
+  eleventyConfig.addFilter('dateReadable', date => {
+      return moment(date).format('LL')
+  });  
+  
+  eleventyConfig.addFilter('datePretty', date => {
+      return moment(date).format('DD MMMM YYYY')
+  });
+
+  eleventyConfig.addFilter('dateReadable', date => {
+      return moment(date).format('LL')
+  });
+  
+ function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
+
+  // Minify HTML
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+  // Let Eleventy transform HTML files as nunjucks
+  // So that we can use .html instead of .njk
+  return {
+    dir: {
+      input: "src",
+      layouts: '_includes/layouts',
+    },
+    htmlTemplateEngine: "njk",
+  };
+};
